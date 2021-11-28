8de38f22ebd549ef985248a5c4c3516775c0528d
Rahulrwt <rahulrawat.rrc@gmail.com>
2021-11-28 19:57:40 +0530

    Portfolio

---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h2 className = 'about_heading'>About Me</h2>
+            <p className = 'para'>Organized, dedicated and ambitious budding computer science professional with excellent attention to detail, and thirst for learning new skills that will help me to compete and level up career.</p>
+            <p>I enjoy turning complex problems into simple, beautiful and intuitive designs. When I'm not pushing pixels, you'll find me watching anime or in the mountains.</p>
+        </div>
+    )
+}
+
+export default About
